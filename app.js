9022001590b676489ab7bdfef2b5d66a5aeb2893
Jherey <jerry4christ2004@yahoo.com>
2018-03-06 21:57:12 +0100

    Change port and add version/route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import routes from './server/routes/index';
 
 // Set up the express app
 const app = express();
@@ -11,6 +12,9 @@ app.use(logger('dev'));
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Versioning and Routes
+app.use('/api/v1/', routes);
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
