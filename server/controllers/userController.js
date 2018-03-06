09cb305d7d523a3178b6e7aa0e89b8d3ddfaa0bf
Jherey <jerry4christ2004@yahoo.com>
2018-03-06 22:40:25 +0100

    Create user login feature

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 import users from '../models/users';
 
 /**
- * @class business
+ * @class users
  */
 class Users {
 	/**
@@ -21,6 +21,29 @@ class Users {
 		return res.json({
 			message: 'User registered successfully',
 			error: false
+		});
+	}
+
+	/**
+   * @returns {Object} loginUser
+   * @param {*} req
+   * @param {*} res
+   */
+	static loginUser(req, res) {
+		const { email, password } = req.body;
+		users.forEach((user) => {
+			if (
+				email === user.email && password === user.password
+			) {
+				return res.status(200).json({
+					message: 'Success',
+					error: false
+				});
+			}
+		});
+		res.status(400).json({
+			message: 'Error logining in',
+			error: true
 		});
 	}
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,8 @@ import Users from '../controllers/userController';
 
 const router = express.Router();
 
+//	Users endpoints
 router.post('/auth/signup', Users.registerUsers);
+router.post('/auth/login', Users.loginUser);
 
 export default router;
