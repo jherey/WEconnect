cbb8f2c49e381f146f2f58ed6a5f1823b07ca89d
Jherey <jerry4christ2004@yahoo.com>
2018-03-06 22:54:11 +0100

    Create register a business feature
    server/controllers/businessController.js defines methods.
    server/models/business.js mimicks a database for businesses.
    server/routes/index.js defines the route and methods.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import Users from '../controllers/userController';
+import Business from '../controllers/businessController';
 
 const router = express.Router();
 
 //	Users endpoints
 router.post('/auth/signup', Users.registerUsers);
 router.post('/auth/login', Users.loginUser);
+
+//	Business endpoints
+router.post('/businesses', Business.registerBusiness);
 
 export default router;
