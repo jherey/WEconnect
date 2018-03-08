4009ac6c4962b77b2280bdd33c23bcd4b38b1436
Jherey <jerry4christ2004@yahoo.com>
2018-03-08 15:28:35 +0100

    Fix issues with Travis

--- server/test/middleware/userValidatorTest.js
+++ server/test/middleware/userValidatorTest.js
@@ -1,12 +1,10 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import spies from 'chai-spies';
-// import expressValidator from 'express-validator';
+//	import spies from 'chai-spies';
 import middleware from '../../middleware/userValidator';
 
 chai.use(chaiHttp);
-chai.use(spies);
-// chai.use(expressValidator);
+//	chai.use(spies);
 const { expect } = chai;
 const signup = middleware.userSignUp;
 const login = middleware.userLogin;
@@ -14,26 +12,26 @@ const login = middleware.userLogin;
 /*
   * Test SIGNUP
   */
-describe('signup tests', () => {
-	it('should return a function()', () => {
-		expect(signup).to.be.a('function');
+describe('MIDDLEWARE TESTS', () => {
+	describe('signup tests', () => {
+		it('should return a function()', () => {
+			expect(signup).to.be.a('function');
+		});
+
+		it('should accept three arguments', () => {
+			expect(signup.length).to.equal(3);
+		});
+
+		// it('should call next() once', () => {
+		// 	const spy = chai.spy(signup);
+		// 	signup('jerry', 'jerry@gmail.com', spy);
+		// 	expect(spy).to.have.been.called();
+		// });
 	});
 
-	it('should accept three arguments', () => {
-		expect(signup.length).to.equal(3);
-	});
-
-	// it('should call next() once', () => {
-	// 	const spy = chai.spy(signup);
-	// 	signup('jerry', 'jerry@gmail.com', spy);
-	// 	expect(spy).to.have.been.called();
-	// });
-});
-
-/*
-  * Test LOGIN
-  */
-describe('my middleware', () => {
+	/*
+		* Test LOGIN
+		*/
 	describe('login tests', () => {
 		it('should return a function()', () => {
 			expect(login).to.be.a('function');
