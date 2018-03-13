cbaebcbaa65acbb4fe084904bd0a38d9a8b11946
Jherey <jerry4christ2004@yahoo.com>
2018-03-13 14:12:06 +0100

    Add model and migration for users

---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define('User', {
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, {});
+	User.associate = (models) => {
+		// associations can be defined here
+	};
+	return User;
+};
