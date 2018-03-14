df1070bc5ee16fc16ffc5f78158c3de98cef6720
Jherey <jerry4christ2004@yahoo.com>
2018-03-14 09:08:48 +0100

    Change route for users to use postgresql and jwt

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,23 +1,11 @@
 import express from 'express';
 import Users from '../controllers/userController';
-import Business from '../controllers/businessController';
 import userValidator from '../middleware/userValidator';
-import businessValidator from '../middleware/businessValidator';
 
 const router = express.Router();
 
 //	Users endpoints
 router.post('/auth/signup', userValidator.userSignUp, Users.registerUsers);
 router.post('/auth/login', userValidator.userLogin, Users.loginUser);
-router.get('/auth/', Users.getAllUsers);
-
-//	Business endpoints
-router.post('/businesses', businessValidator.registerBusiness, Business.registerBusiness);
-router.put('/businesses/:businessId', Business.updateBusiness);
-router.delete('/businesses/:businessId', Business.removeBusiness);
-router.get('/businesses/:businessId', Business.getABusiness);
-router.get('/businesses', businessValidator.query, Business.getAllBusinesses);
-router.post('/businesses/:businessId/reviews', Business.addReview);
-router.get('/businesses/:businessId/reviews', Business.getAllReviews);
 
 export default router;
