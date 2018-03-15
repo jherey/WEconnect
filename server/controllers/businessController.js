9a9ed3d3d403b77381e830342b93a77ed248f09f
Jherey <jerry4christ2004@yahoo.com>
2018-03-15 15:25:32 +0100

    Add register business route

--- server/controllers/businessController.js
+++ server/controllers/businessController.js
@@ -1,123 +1,63 @@
-import business from '../models/business';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import models from '../models/index';
 
-const successMessage = (res, message) => res.status(200).json({
-	message,
-	error: false
-});
-
-const errorMessage = (res, message) => res.status(400).json({
-	message,
-	error: true
-});
+dotenv.config();
+//	Secret key
+const secret = process.env.secretKey;
+//	Business model
+const businesses = models.Business;
 
 /**
  * @class business
  */
 class Business {
 	/**
- * @returns {Object} getAllBusinesses
- * @param {*} req
- * @param {*} res
- */
-	static getAllBusinesses(req, res) {
-		return res.json({
-			business
-		});
-	}
-
-	/**
    * @returns {Object} registerBusiness
    * @param {*} req
    * @param {*} res
+	 * @returns {json} json
    */
 	static registerBusiness(req, res) {
-		business.push(req.body);
-		return successMessage(res, 'Registered business successfully');
-	}
-
-	/**
-   * @returns {Object} updateBusiness
-   * @param {*} req
-   * @param {*} res
-   */
-	static updateBusiness(req, res) {
-		for (let i = 0; i < business.length; i += 1) {
-			if (business[i].id === parseInt(req.params.businessId, 10)) {
-				business[i].name = req.body.name;
-				business[i].address = req.body.address;
-				business[i].website = req.body.website;
-				return successMessage(res, 'Business successfully updated');
-			}
-		}
-		return errorMessage(res, 'Business not found');
-	}
-
-	/**
-   * @returns {Object} removeBusiness
-   * @param {*} req
-   * @param {*} res
-   */
-	static removeBusiness(req, res) {
-		for (let i = 0; i < business.length; i += 1) {
-			if (business[i].id === parseInt(req.params.businessId, 10)) {
-				business.splice(business[i], 1);
-				return successMessage(res, 'Business successfully deleted');
-			}
-		}
-		return errorMessage(res, 'Business cannot be deleted because it does not exist');
-	}
-
-	/**
-   * @returns {Object} getABusiness
-   * @param {*} req
-   * @param {*} res
-   */
-	static getABusiness(req, res) {
-		for (let i = 0; i < business.length; i += 1) {
-			if (business[i].id === parseInt(req.params.businessId, 10)) {
-				return res.json({
-					business: business[business[i].id - 1],
-					message: 'Success',
-					error: false
+		const {
+			busname, website, telephone, category, businfo, email, busimage, location
+		} = req.body;
+		//	Change location and category to lowercase
+		const loc = location.toLowerCase();
+		const cat = category.toLowerCase();
+		//	Verify the user and issue a token
+		jwt.verify(req.token, secret, (err, authData) => {
+			if (err) {
+				//	Wrong token
+				res.status(403).json({
+					message: 'Token unmatch'
 				});
+			} else {
+				//	Create the business
+				businesses
+					.create({
+						busname,
+						website,
+						telephone,
+						category: cat,
+						businfo,
+						email,
+						busimage,
+						location: loc,
+						userId: authData.id	//	Get the id of the user from the authData in the token
+					})
+					//	Success message
+					.then(business => res.status(201).json({
+						message: 'Business created successfully',
+						business,
+						authData
+					}))
+					//	Any errors
+					.catch(() => res.status(500).json({
+						message: 'Internal server error'
+					}));
 			}
-		}
-		return errorMessage(res, 'Business not found');
-	}
-
-	/**
-   * @returns {Object} addReview
-   * @param {*} req
-   * @param {*} res
-   */
-	static addReview(req, res) {
-		const { review } = req.body;
-		for (let i = 0; i < business.length; i += 1) {
-			if (business[i].id === parseInt(req.params.businessId, 10)) {
-				business[i].reviews.push(review);
-				return res.json({
-					message: 'Review sucessfully added',
-					business: business[i].reviews
-				});
-			}
-		}
-		return errorMessage(res, 'Review cannot be added to business that does not exist');
-	}
-
-	/**
-   * @returns {Object} getAllReviews
-   * @param {*} req
-   * @param {*} res
-   */
-	static getAllReviews(req, res) {
-		for (let i = 0; i < business.length; i += 1) {
-			if (business[i].id === parseInt(req.params.businessId, 10)) {
-				return res.json({
-					Reviews: business[i].reviews
-				});
-			}
-		}
-		return errorMessage(res, 'Business that does not exist, review cannot be gotten');
+		});
 	}
 }
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import Users from '../controllers/userController';
+import Business from '../controllers/businessController';
 import userValidator from '../middleware/userValidator';
+import businessValidator from '../middleware/businessValidator';
 
 const router = express.Router();
 
 //	Users endpoints
 router.post('/auth/signup', userValidator.userSignUp, Users.registerUsers);
 router.post('/auth/login', userValidator.userLogin, Users.loginUser);
+
+//	Business endpoints
+router.post('/businesses', businessValidator.registerBusiness, businessValidator.verifyToken, Business.registerBusiness);
 
 export default router;
