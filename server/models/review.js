83052552af78b0d53310d72c0ae3ee9cce6a4773
Jherey <jerry4christ2004@yahoo.com>
2018-03-15 01:35:13 +0100

    Add model and migration for reviews

---
+++ server/models/review.js
@@ -0,0 +1,53 @@
+const reviewModel = (sequelize, DataTypes) => {
+	const Review = sequelize.define('Review', {
+		review: {
+			type: DataTypes.TEXT,
+			allowNull: false
+		},
+		userId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					args: true,
+					msg: 'UserId Field Required!',
+				},
+			},
+			onDelete: 'CASCADE',
+			references: {
+				model: 'event',
+				Key: 'id',
+				as: 'userId',
+			},
+		},
+		businessId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					args: true,
+					msg: 'BusinessId Field Required!',
+				},
+			},
+			onDelete: 'CASCADE',
+			references: {
+				model: 'review',
+				Key: 'id',
+				as: 'businessId',
+			},
+		},
+	});
+	Review.associate = (models) => {
+		Review.belongsTo(models.Business, {
+			foreignKey: 'businessId',
+			onDelete: 'CASCADE',
+		});
+
+		Review.belongsTo(models.User, {
+			foreignKey: 'userId',
+		});
+	};
+	return Review;
+};
+
+export default reviewModel;
