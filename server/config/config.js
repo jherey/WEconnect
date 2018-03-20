95c1596a85ca029076c81cb12643d815e28047b7
Jherey <jerry4christ2004@yahoo.com>
2018-03-20 02:25:51 +0100

    Add env variable to production script

--- server/config/config.js
+++ server/config/config.js
@@ -12,12 +12,12 @@ module.exports = {
 	test: {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
+		database: process.env.DBNAME,
 		host: '127.0.0.1',
 		port: 5432,
 		dialect: 'postgres'
 	},
 	production: {
-		environment: 'production'
+		use_env_variable: 'DATABASE_URL'
 	}
 };
