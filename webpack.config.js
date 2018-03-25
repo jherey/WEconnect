1ac6ac984f7ed1066795e808909c6cec98c3c0f7
Jherey <jerry4christ2004@yahoo.com>
2018-03-25 12:27:30 +0100

    Include loaders for js and scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: {
     index: './client/index.js'
@@ -7,5 +9,32 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ExtractTextWebpackPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        }),
+        exclude: /node_modules/
+      }
+    ]
   }
 };
