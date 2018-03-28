218dd03faad79f4c66bfd48181e8fcdfca2e19ba
Jherey <jerry4christ2004@yahoo.com>
2018-03-28 19:20:12 +0100

    Adds more user tests

--- server/test/controllers/a_userControllerTest.js
+++ server/test/controllers/a_userControllerTest.js
@@ -19,9 +19,45 @@ describe('User', () => {
       .post('/api/v1/auth/signup')
       .send(userDetails)
       .end((err, res) => {
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.have.property('message');
-        expect(res.body).to.have.property('error');
+        expect(res.body).to.have.property('message')
+          .eql('Firstname is required');
+        expect(res.status).to.equal(400);
+        done();
+      });
+  });
+
+  it('it should not register a user', (done) => {
+    // HTTP POST -> DONT REGISTER A NEW USER
+    const userDetails = {
+      firstname: 'Jeremiah',
+      username: 'Seyih',
+      password: 'olufayo'
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(userDetails)
+      .end((err, res) => {
+        expect(res.body).to.have.property('message')
+          .eql('Lastname is required');
+        expect(res.status).to.equal(400);
+        done();
+      });
+  });
+
+  it('it should not register a user', (done) => {
+    // HTTP POST -> DONT REGISTER A NEW USER
+    const userDetails = {
+      firstname: 'Jeremiah',
+      lastname: 'Oluwaseyi',
+      username: 'Seyih',
+      password: 'olufayo'
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(userDetails)
+      .end((err, res) => {
+        expect(res.body).to.have.property('message')
+          .eql('Email is required');
         expect(res.status).to.equal(400);
         done();
       });
@@ -42,7 +78,6 @@ describe('User', () => {
       .post('/api/v1/auth/signup')
       .send(userDetails)
       .end((err, res) => {
-        expect(res.body).to.be.a('object');
         expect(res.body).to.have.property('message')
           .eql('Signed up successfully');
         expect(res.status).to.equal(201);
@@ -59,9 +94,24 @@ describe('User', () => {
       .post('/api/v1/auth/login')
       .send(userDetails)
       .end((err, res) => {
-        expect(res.body).to.be.a('object');
         expect(res.body).to.have.property('message')
           .eql('Password field is empty');
+        expect(res.status).to.equal(400);
+        done();
+      });
+  });
+
+  it('it should not login a user', (done) => {
+    // HTTP POST -> DON'T LOGIN A USER
+    const userDetails = {
+      password: 'jherey'
+    };
+    chai.request(app)
+      .post('/api/v1/auth/login')
+      .send(userDetails)
+      .end((err, res) => {
+        expect(res.body).to.have.property('message')
+          .eql('Username field is empty');
         expect(res.status).to.equal(400);
         done();
       });
