bd72dc379ae12e82f70298ecb703e5e5ee5734e6
Jherey <jerry4christ2004@yahoo.com>
2018-04-04 05:26:17 +0100

    Adds links to index.js

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Root from '../components/Root';
+import Signin from '../components/Signin';
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<div>
+					<ul>
+						<li><Link to={'/'}>Home</Link></li>
+						<li><Link to={'/Login'}>Signin</Link></li>
+					</ul>
+					<hr />
+
+					<Switch>
+						<Route exact path='/' component={Root} />
+						<Route exact path='/Login' component={Signin} />
+					</Switch>
 				</div>
 			</Router>
 		);
