6311ab4c03e1c182f4395579099f85a0b44d46ff
Jherey <jerry4christ2004@yahoo.com>
2018-04-04 09:44:03 +0100

    Add navbar component

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
 		return (
 			<nav class="navbar navbar-custom navbar-expand-lg navbar-dark">
 				<div class="container">
-					<Link className="navbar-brand" to="/home">WEconnect</Link>
+					<Link className="navbar-brand" to="/index">WEconnect</Link>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
 						aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
@@ -15,7 +15,7 @@ class Navbar extends Component {
 					<div class="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul class="navbar-nav mr-auto nav-pills">
 							<li class="nav-item">
-								<a class="nav-link" href="contact.html">CONTACT</a>
+								<a class="nav-link" href="contact.html">CONTACT US</a>
 							</li>
 						</ul>
 						<ul class="navbar-nav ml-auto">
