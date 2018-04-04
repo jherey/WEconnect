fc3eebc19e5dde5e766f286e53298340f18a63cf
Jherey <jerry4christ2004@yahoo.com>
2018-04-04 12:59:14 +0100

    Adds route to root file

--- client/components/Root.js
+++ client/components/Root.js
@@ -5,6 +5,7 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Signin from './Signin';
 import Signup from './Signup';
+import Register from './Register';
 
 class Root extends Component {
 	render() {
@@ -16,6 +17,7 @@ class Root extends Component {
 						<Route path="/index" exact component={Home} />
 						<Route path="/signin" exact component={Signin} />
 						<Route path="/signup" exact component={Signup} />
+						<Route path="/register" exact component={Register} />
 					</Switch>
 				</main>
 				<Footer />
