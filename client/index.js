337b01179cf3be33dced2bb4e8c8eca32e918899
Jherey <jerry4christ2004@yahoo.com>
2018-04-09 16:28:23 +0100

    Creates store for app

--- client/index.js
+++ client/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import Routes from './Routes';
 import styles from './public/styles/index.scss';
 
+const store = createStore(
+	(state = {}) => state,
+	applyMiddleware(thunk)
+);
+
 render(
-	<Routes />, document.getElementById('app')
+	<Provider store={store}>
+		<Routes />
+	</Provider>
+, document.getElementById('app')
 );
