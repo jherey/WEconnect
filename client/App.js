b865be98bcbd218298ca8cbb4055f6c17b1e05cb
Jherey <jerry4christ2004@yahoo.com>
2018-04-09 14:36:28 +0100

    Adds edit business route to homepage

---
+++ client/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Register from './components/Register';
+import EditBusiness from './components/EditBusiness';
+import BusinessProfile from './components/BusinessProfile';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="">
+				<Navbar />
+				<main>
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/signin" exact component={Signin} />
+						<Route path="/signup" exact component={Signup} />
+						<Route path="/register" exact component={Register} />
+						<Route path="/edit" exact component={EditBusiness} />
+						<Route path="/business" exact component={BusinessProfile} />
+					</Switch>
+				</main>
+				<Footer />
+			</div>
+		);
+	}
+}
+
+export default App;
