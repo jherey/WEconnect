c3ef5908ee4d08f4a0cc5acc4265f2111d0236e2
Jherey <jerry4christ2004@yahoo.com>
2018-04-11 22:34:57 +0100

    Adds flash message action to signup form

--- client/components/Signup/Signup.js
+++ client/components/Signup/Signup.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import SignupForm from './SignupForm';
 import { signupUser } from '../../actions/userActions';
+import { addFlashMessage } from '../../actions/flashMessages';
 
 class Signup extends Component {
 	render() {
-		const {signupUser } = this.props;
-		const { context } = this.props;
+		const { signupUser, addFlashMessage } = this.props;
 		return (
 			<div>
-				<SignupForm signupUser={signupUser} />
+				<SignupForm signupUser={signupUser} addFlashMessage={addFlashMessage} />
 			</div>
 		);
 	}
 };
 
-export default connect(null, { signupUser })(Signup);
+export default connect(null, { signupUser, addFlashMessage })(Signup);
