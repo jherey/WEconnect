62a7a339690b976595ee30c8de743dab5eb23068
Jherey <jerry4christ2004@yahoo.com>
2018-04-11 22:31:39 +0100

    Adds redux dev tool to store

--- client/index.js
+++ client/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import Routes from './Routes';
 import allReducers from './reducers';
 
 const store = createStore(
 	allReducers,
-	applyMiddleware(thunk)
+	compose(
+		applyMiddleware(thunk),
+		window.devToolsExtension ? window.devToolsExtension() : f => f
+	)
 );
 
 render(
