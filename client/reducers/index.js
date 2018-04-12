75532686df3e8d8a6b8ee9411a011469c20fd98a
Jherey <jerry4christ2004@yahoo.com>
2018-04-12 02:49:53 +0100

    Adds logged in user to store

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import flashMessages from './reducer-flashMessages';
+import user from './reducer-user';
 
 const allReducers = combineReducers({
-  flashMessages
+  flashMessages,
+  user
 });
 
 export default allReducers;
