c180898e20cbcaef93542852142f87c22cfefe24
Jherey <jerry4christ2004@yahoo.com>
2018-04-12 02:47:07 +0100

    Writes action to store user data in store

--- client/actions/userActions.js
+++ client/actions/userActions.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import jwt from 'jsonwebtoken';
+import setAuthToken from './setAuthToken';
 
 /**
  * @returns {Object} promise
@@ -8,4 +10,30 @@ export function signupUser(userData) {
   return dispatch => {
     return axios.post('api/v1/auth/signup', userData);
   }
+}
+
+/**
+ * @returns {Object} promise
+ * @param {*} userData
+ */
+export function signinUser(userData) {
+  return dispatch => {
+    return axios.post('api/v1/auth/login', userData).then(res => {
+      const token = res.data.token;
+      localStorage.setItem('token', token);
+      setAuthToken(token);
+      dispatch(setCurrentUser(jwt.decode(token)));
+    });
+  }
+}
+
+/**
+ * @returns {Object} promise
+ * @param {*} user
+ */
+export function setCurrentUser(user) {
+  return {
+    type: 'SET_CURRENT_USER',
+    user
+  };
 }
