112cad0da4db286ba512f194d80000725ce94656
Jherey <jerry4christ2004@yahoo.com>
2018-04-13 07:20:35 +0100

    Changes reducer to store one flash message

--- client/reducers/reducer-flashMessages.js
+++ client/reducers/reducer-flashMessages.js
@@ -1,16 +1,13 @@
 import shortid from 'shortid';
 
-export default (state = [], action = {}) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case 'ADD_FLASH_MESSAGE':
-      return [
-        ...state,
-        {
+      return {
           id: shortid.generate(),
           type: action.message.type,
           text: action.message.text
         }
-      ];
     default:
       return state;
   }
