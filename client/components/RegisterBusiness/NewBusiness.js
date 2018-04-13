41c5eb503d66d412c2e8396567e504158d71ddbe
Jherey <jerry4christ2004@yahoo.com>
2018-04-13 07:51:18 +0100

    Adds action creator to container

--- client/components/RegisterBusiness/NewBusiness.js
+++ client/components/RegisterBusiness/NewBusiness.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import NewBusinessForm from './NewBusinessForm';
 import { createBusiness } from '../../actions/businessActions';
+import { addFlashMessage } from '../../actions/flashMessages';
 
 class NewBusiness extends Component {
 	render() {
 		const { createBusiness } = this.props;
 		return (
 			<div>
-				<NewBusinessForm createBusiness={createBusiness} />
+				<NewBusinessForm createBusiness={createBusiness} addFlashMessage={addFlashMessage} />
 			</div>
 		);
 	}
 }
 
-export default connect(null, { createBusiness })(NewBusiness);
+export default connect(null, { createBusiness, addFlashMessage })(NewBusiness);
