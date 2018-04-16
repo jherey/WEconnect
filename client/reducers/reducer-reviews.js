33e32f5330e941ad591747efbfa8e0b27b02fbc1
Jherey <jerry4christ2004@yahoo.com>
2018-04-16 20:12:20 +0100

    Adds a case for reducer to update reviews state

--- client/reducers/reducer-reviews.js
+++ client/reducers/reducer-reviews.js
@@ -2,6 +2,13 @@ export default function (state = [], action = {}) {
 	switch (action.type) {
 		case 'GET_REVIEWS':
 			return action.reviews
+
+		case 'POST_REVIEW':
+			return [
+				...state,
+				action.review
+			]
+			
 		default:
 			return state;
 	}
