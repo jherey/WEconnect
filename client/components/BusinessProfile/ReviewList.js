65aa3807143ff7d9818be238edf386a6d4e60494
Jherey <jerry4christ2004@yahoo.com>
2018-04-16 20:10:09 +0100

    Edits review list to have state and add an onSubmit that calls an action

--- client/components/BusinessProfile/ReviewList.js
+++ client/components/BusinessProfile/ReviewList.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react';
 import Review from './Review.js';
+import { connect } from 'react-redux';
+import { addReview } from '../../actions/reviewActions';
+import { addFlashMessage } from '../../actions/flashMessages';
 
 class ReviewList extends Component {
+	constructor() {
+		super();
+		this.state = {
+			review: '',
+			errors: '',
+			isLoading: ''
+		}
+
+		this.onReviewChange = this.onReviewChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+
+	onReviewChange(e) {
+		this.setState({ review: e.target.value })
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		this.setState({ errors: '', isLoading: true });
+		this.props.addReview(this.props.id, this.state).then(
+			() => {
+				document.getElementById("hidePopUpBtn").click();
+				this.props.addFlashMessage({
+					type: 'success',
+					text: 'Review posted'
+				});
+			},
+			(data) => this.setState({ errors: data.response.data.message, isLoading: false })
+		);
+		this.setState({ review: '' });
+	}
+
 	render() {
 		const { reviews } = this.props;
 		const reviewComponent = reviews.map(review => {
@@ -28,21 +63,30 @@ class ReviewList extends Component {
 					<div className="modal-dialog" role="document">
 						<div className="modal-content">
 							<div className="modal-header">
-								<h5 className="modal-title" id="exampleModalLabel">Write Review</h5>
-								<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+								<h5 className="modal-title" id="exampleModalLabel">Add Review</h5>
+								<button id="hidePopUpBtn" type="button" className="close" data-dismiss="modal" aria-label="Close">
 									<span aria-hidden="true">&times;</span>
 								</button>
 							</div>
-							<div className="modal-body">
-								<div className="form-group">
-									<label htmlFor="comment" id="col">Review:</label>
-									<textarea className="form-control" rows="5" id="review"></textarea>
+							<form onSubmit={this.handleSubmit}>
+								<div className="modal-body">
+									<div className="form-group">
+										<label htmlFor="comment" id="col">Review:</label>
+										<textarea
+											onChange={this.onReviewChange}
+											name='review'
+											className="form-control"
+											rows="5"
+											id="review"
+										>
+										</textarea>
+									</div>
 								</div>
-							</div>
-							<div className="modal-footer">
-								<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-								<button type="button" className="btn btn-primary">Save review</button>
-							</div>
+								<div className="modal-footer">
+									<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+									<button type="submit" className="btn btn-primary">Save review</button>
+								</div>
+							</form>
 						</div>
 					</div>
 				</div>
@@ -51,4 +95,4 @@ class ReviewList extends Component {
 	}
 }
 
-export default ReviewList;
+export default connect(null, { addReview, addFlashMessage })(ReviewList);
