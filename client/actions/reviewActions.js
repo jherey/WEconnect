c7f9d64dbfc7a7b857a324caf89f952307daf2c5
Jherey <jerry4christ2004@yahoo.com>
2018-04-16 20:11:50 +0100

    Action creator makes api call and dispatches a function to the reducer

--- client/actions/reviewActions.js
+++ client/actions/reviewActions.js
@@ -15,3 +15,19 @@ export function fetchReviews(id) {
 			})
 	}
 }
+
+export function postReview(review) {
+	return {
+		type: 'POST_REVIEW',
+		review
+	}
+}
+
+export function addReview(id, review) {
+	return dispatch => {
+		return axios.post(`http://localhost:8000/api/v1/businesses/${id}/reviews`, review)
+			.then(review => {
+				dispatch(postReview(review.data.createdReview));
+			})
+	}
+}
