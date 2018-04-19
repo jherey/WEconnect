0cf5ed4cea6666050c76c5d9d7042e7cf5f311c0
Jherey <jerry4christ2004@yahoo.com>
2018-04-19 11:22:01 +0100

    Adds navbar component for registered and non-registered users

---
+++ client/components/Navbar/NavbarComponent.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class NavbarComponent extends Component {
+	constructor() {
+		super();
+		this.state = {
+			keyword: '',
+			type: '',
+			errors: '',
+			isLoading: false
+		}
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+		this.setState({ errors: '', isLoading: true });
+		this.props.search(this.state.keyword, this.state.type).then(
+			() => {
+				this.context.router.history.push('/search');
+			},
+			(data) => this.setState({ errors: data.response.data.message, isLoading: false })
+		)
+	}
+
+	render() {
+		const { isAuthenticated } = this.props.authUser;
+
+		const authUserLinks = (
+			<ul className="navbar-nav">
+				<li className="nav-item">
+					<Link className="nav-link" to="/register">Register a Business</Link>
+				</li>
+				<li className="nav-item mr-5">
+					<a className="nav-link" href="#" onClick={this.props.signout}>Sign out</a>
+				</li>
+			</ul>
+		);
+
+		const guestLinks = (
+			<ul className="navbar-nav">
+				<li className="nav-item">
+					<Link className="nav-link" to="/signin">Login</Link>
+				</li>
+				<li className="nav-item mr-5">
+					<Link className="nav-link" to="/signup">Sign Up</Link>
+				</li>
+			</ul>
+		);
+
+		return (
+			<div id='nav'>
+				<nav className="navbar navbar-custom navbar-expand-lg navbar-dark">
+					<Link className="navbar-brand ml-5" to="/">WeConnect</Link>
+					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div id="navbarNavDropdown" className="navbar-collapse collapse">
+						<ul className="navbar-nav mx-auto">
+							<li className="nav-item">
+								<form onSubmit={this.onSubmit}>
+									<div className="input-group mr-3" id="content">
+										<input
+											value={this.state.keyword}
+											onChange={this.onChange}
+											name="keyword"
+											type="text"
+											size="40"
+											className="form-control"
+											placeholder="Search by location or category"
+											aria-describedby="btnGroupAddon"
+										/>
+										<select
+											className='form-control'
+											name='type'
+											onChange={this.onChange}
+											value={this.state.type}
+										>
+											<option value='' disabled>Choose...</option>
+											<option value='location'>Location</option>
+											<option value='category'>Category</option>
+										</select>
+										<button type="submit" id="search" className="btn button">Search</button>
+									</div>
+								</form>
+							</li>
+						</ul>
+						{isAuthenticated ? authUserLinks : guestLinks}
+					</div>
+				</nav>
+			</div>
+		);
+	}
+}
+
+NavbarComponent.contextTypes = {
+	router: PropTypes.object.isRequired
+}
+
+export default NavbarComponent;
