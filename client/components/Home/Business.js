5349eda2dc80e0d40202d889e93ae30fe4171da7
Jherey <jerry4christ2004@yahoo.com>
2018-04-22 11:38:30 +0100

    Changes business to card

--- client/components/Home/Business.js
+++ client/components/Home/Business.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Business = (props) => {
-	const { id, name } = props;
+	const { id, name, description, address, location, category } = props;
 	return (
-		<div className="col-lg-3 col-sm-6">
-			<Link to={`/${id}`} className="d-block mb-4 h-100">
-				<img className="img-fluid img-thumbnail" src={require('../../public/images/companies/nbc.jpg')} alt="" />
-				<p className="category">{name}</p>
-			</Link>
+		<div className="col-lg-3 col-md-6">
+			<div className='card mb-4 testimonial-card'>
+				<div class="avatar mx-auto white">
+					<img className="rounded-circle" src={require('../../public/images/companies/nbc.jpg')} alt="" />
+    		</div>
+				<div className='card-body'>
+					<p className="category text-center">{name}</p>
+					<hr />
+					<p class="card-text"><small>Description: {description}</small></p>
+					<Link to={`/${id}`} className="btn btn-orange d-block mb-1">
+						View Profile
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 }
