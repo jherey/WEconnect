fba6a61375e331bb635dda21d8c861bb9148117a
Jherey <jerry4christ2004@yahoo.com>
2018-04-27 04:45:31 +0100

    Creates a spinner component for loading

---
+++ client/components/Spinner/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import puff from './puff.svg';
+
+const Spinner = () =>
+	<div style={{ marginTop: '20%', marginBottom: '20%', textAlign: 'center' }}>
+		<img src={puff} />
+	</div>;
+
+export default Spinner;
