74bc64f841c8f6141f293e07f282bb1905a894a8
Jherey <jerry4christ2004@yahoo.com>
2018-05-04 17:42:52 +0100

    Makes business card of equal height

--- client/components/Home/Business.js
+++ client/components/Home/Business.js
@@ -5,7 +5,7 @@ const Business = (props) => {
 	const { id, name, description, address, businessImage, location, category } = props;
 	return (
 		<div className="col-lg-3 col-md-6 py-2">
-			<div className='card'>
+			<div className='card h-100'>
 				<div className="avatar mx-auto white">
 					<img
 						className="rounded-circle mt-2"
