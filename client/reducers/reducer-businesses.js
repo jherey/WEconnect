b71b314e8be9190d2056e28fe1b0420ee60aae99
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2018-05-10 19:54:05 +0100

    Add JS Doc to all reducers

--- client/reducers/reducer-businesses.js
+++ client/reducers/reducer-businesses.js
@@ -1,26 +1,34 @@
+/**
+ * @description - Business reducer
+ *
+ * @param {*} state
+ * @param {*} action
+ *
+ * @returns { business } - State
+ */
 export default function businesses(state = [], action = {}) {
-	switch (action.type) {
-		case 'ADD_BUSINESS':
-			return [
-				...state,
-				action.business
-			]
+  switch (action.type) {
+    case 'ADD_BUSINESS':
+      return [
+        ...state,
+        action.business
+      ];
 
-		case 'UPDATE_BUSINESS':
-			return state.map(item => {
-				if (item.id === action.updatedBusiness) {
-					return action.business;
-				}
-				return item;
-			})
+    case 'UPDATE_BUSINESS':
+      return state.map((item) => {
+        if (item.id === action.updatedBusiness) {
+          return action.business;
+        }
+        return item;
+      });
 
-		case 'DELETE_BUSINESS':
-			return state.filter(item => item.id !== action.businessId);
+    case 'DELETE_BUSINESS':
+      return state.filter(item => item.id !== action.businessId);
 
-		case 'SET_BUSINESSES':
-			return action.businesses
-			
-		default:
-			return state;
-	}
+    case 'SET_BUSINESSES':
+      return action.businesses;
+
+    default:
+      return state;
+  }
 }
