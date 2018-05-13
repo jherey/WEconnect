a721bac68482aca9aa0a1d140ae9ae4bbba5d2ca
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2018-05-13 07:22:41 +0100

    Removes line beneath text

--- client/components/Home/HeroImage.js
+++ client/components/Home/HeroImage.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 
 const HeroImage = () => {
 	return (
-		<div className="main-image">
+		<div className="hero-image">
 			<div className="row">
 				<div className="col-lg-12">
 					<div className="text-center catchphrase">
 						<h3>Connecting People, Connecting Businesses</h3>
-						<hr id="index-hr" />
 					</div>
 				</div>
 			</div>
