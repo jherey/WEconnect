aef95d4d97a284f5b7f0e3bf50d660dba2175856
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2018-05-19 12:59:51 +0100

    UI changes for form components

--- client/components/Signin/SigninForm.js
+++ client/components/Signin/SigninForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-// import Materialize from 'materialize-css';
+import { Link } from 'react-router-dom';
 import Spinner from '../Spinner';
 
 class SigninForm extends Component {
@@ -47,48 +47,100 @@ class SigninForm extends Component {
 		if(isLoading) { return <Spinner />; }
 
 		return (
-			<div className="signup">
-				<div className="login-form col-md-4 offset-md-4">
-					<h1 className="title">Sign In</h1>
-					{errors === 'Username/Password Incorrect' && <div className='alert alert-danger'>{errors}</div>}
-					<form onSubmit={this.onSubmit}>
-						<div>
-							<label>Username</label>
-							<input
-								value={username}
-								onChange={this.onChange}
-								type="text"
-								name="username"
-								className="form-control"
-							/>
-							{errors === 'Username is required' && <div className='alert alert-danger'>{errors}</div>}
+			<div className="form-signin">
+				<div className="login-form container py-5">
+					<h1 className="text-center" style={{'color': 'white'}}>Sign In</h1>
+					<div className="row">
+						<div className="col-md-10 mx-auto">
+							{errors === 'Username/Password Incorrect' && <div className='alert alert-danger'>{errors}</div>}
+							<form onSubmit={this.onSubmit}>
+								<div className="form-group row">
+									<div className="col-sm-12">
+										<label>Username</label>
+										<input
+											value={username}
+											onChange={this.onChange}
+											type="text"
+											name="username"
+											className="form-control"
+										/>
+										{errors === 'Username is required' && <div className='alert alert-danger'>{errors}</div>}
+									</div>
+								</div>
+								<div className="form-group row">
+									<div className="col-sm-12">
+										<label>Password</label>
+										<input
+											value={password}
+											onChange={this.onChange}
+											type="password"
+											name="password"
+											className="form-control"
+										/>
+										{errors === 'Password is required' && <div className='alert alert-danger'>{errors}</div>}
+										{errors === 'Minimum password length is 5 characters' && <div className='alert alert-danger'>{errors}</div>}
+									</div>
+								</div>
+								<div className="form-group row">
+									<div className="col-sm-12">
+										<button
+											id="signup"
+											className="btn btn-orange btn-lg"
+											disabled={isLoading}
+										>
+											Login
+										</button>
+										<p id="signin-link">Don't have an account?<span><Link to="/signup"> Sign Up</Link></span></p>
+									</div>
+								</div>
+							</form>
 						</div>
-						<div>
-							<label>Password</label>
-							<input
-								value={password}
-								onChange={this.onChange}
-								type="password"
-								name="password"
-								className="form-control"
-							/>
-							{errors === 'Password is required' && <div className='alert alert-danger'>{errors}</div>}
-							{errors === 'Minimum password length is 5 characters' && <div className='alert alert-danger'>{errors}</div>}
-						</div>
-						<div>
-							<button
-								id="signup"
-								className="btn btn-orange btn-lg"
-								disabled={isLoading}
-							>
-								Login
-							</button>
-						</div>
-					</form>
+					</div>
 				</div>
-				<br />
-				<br />
 			</div>
+			// <div className="form">
+			// 	<h1 className="text-center" style={{'color': 'white'}}>Sign In</h1>
+			// 	<div className="login-form col-md-4 offset-md-4">
+					
+			// 		{errors === 'Username/Password Incorrect' && <div className='alert alert-danger'>{errors}</div>}
+			// 		<form onSubmit={this.onSubmit}>
+			// 			<div>
+			// 				<label>Username</label>
+			// 				<input
+			// 					value={username}
+			// 					onChange={this.onChange}
+			// 					type="text"
+			// 					name="username"
+			// 					className="form-control"
+			// 				/>
+			// 				{errors === 'Username is required' && <div className='alert alert-danger'>{errors}</div>}
+			// 			</div>
+			// 			<div>
+			// 				<label>Password</label>
+			// 				<input
+			// 					value={password}
+			// 					onChange={this.onChange}
+			// 					type="password"
+			// 					name="password"
+			// 					className="form-control"
+			// 				/>
+			// 				{errors === 'Password is required' && <div className='alert alert-danger'>{errors}</div>}
+			// 				{errors === 'Minimum password length is 5 characters' && <div className='alert alert-danger'>{errors}</div>}
+			// 			</div>
+			// 			<div>
+			// 				<button
+			// 					id="signup"
+			// 					className="btn btn-orange btn-lg"
+			// 					disabled={isLoading}
+			// 				>
+			// 					Login
+			// 				</button>
+			// 			</div>
+			// 		</form>
+			// 	</div>
+			// 	<br />
+			// 	<br />
+			// </div>
 		);
 	}
 }
--- client/components/Signup/SignupForm.js
+++ client/components/Signup/SignupForm.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { storage } from '../firebase';
 import Spinner from '../Spinner';
 
@@ -67,109 +68,122 @@ class SignupForm extends Component {
 		if (isLoading) { return <Spinner />; }
 
 		return (
-			<div className="signup">
-				<div className="login-form col-md-4 offset-md-4">
-					<h1 className="title">Create Account</h1>
-					{errors === 'Username already exists' && <div className='alert alert-danger'>{errors}</div>}
-					{errors === 'Email address taken' && <div className='alert alert-danger'>{errors}</div>}
-					<form onSubmit={this.onSubmit}>
-						<div>
-							<label className='control-label'>First Name</label>
-							<input
-								value={firstname}
-								onChange={this.onChange}
-								type="text"
-								name="firstname"
-								className="form-control"
-							/>
-							{errors === 'Firstname is required' && <div className='alert alert-danger'>{errors}</div>}
+			<div className="form-signup">
+				<div className="signup-form container py-5">
+					<h1 className="text-center" style={{'color': 'white'}}>Sign Up</h1>
+					<div className="row">
+						<div className="col-md-10 mx-auto">
+							{errors === 'Username already exists' && <div className='alert alert-danger'>{errors}</div>}
+							{errors === 'Email address taken' && <div className='alert alert-danger'>{errors}</div>}
+							<form onSubmit={this.onSubmit}>
+								<div className="form-group row">
+									<div className="col-sm-6">
+										<label>First Name</label>
+										<input
+											value={firstname}
+											onChange={this.onChange}
+											type="text"
+											name="firstname"
+											className="form-control"
+										/>
+										{errors === 'Firstname is required' && <div className='alert alert-danger'>{errors}</div>}
+									</div>
+									<div className="col-sm-6">
+										<label>Last Name</label>
+										<input
+											value={lastname}
+											onChange={this.onChange}
+											type="text"
+											name="lastname"
+											className="form-control"
+										/>
+										{errors === 'Lastname is required' && <div className='alert alert-danger'>{errors}</div>}
+									</div>
+								</div>
+								<div className="form-group row">
+									<div className="col-sm-6">
+									<label className='control-label'>Username</label>
+									<input
+										value={username}
+										onChange={this.onChange}
+										type="text"
+										name="username"
+										className="form-control"
+									/>
+									{errors === 'Username is required' && <div className='alert alert-danger'>{errors}</div>}
+									</div>
+									<div className="col-sm-6">
+										<label className='control-label'>Email</label>
+										<input
+											value={email}
+											onChange={this.onChange}
+											type="email"
+											name="email"
+											className="form-control"
+										/>
+										{errors === 'Email is required' && <div className='alert alert-danger'>{errors}</div>}
+									</div>
+								</div>
+								<div className="form-group row">
+									<div className="col-sm-6">
+										<label className='control-label'>Password</label>
+										<input
+											value={password}
+											onChange={this.onChange}
+											type="password"
+											name="password"
+											className="form-control"
+										/>
+										{errors === 'Password is required' && <div className='alert alert-danger'>{errors}</div>}
+										{errors === 'Minimum password length is 5 characters' && <div className='alert alert-danger'>{errors}</div>}
+									</div>
+									<div className="col-sm-6">
+										<label className='control-label'>Confirm Password</label>
+										<input
+											value={confirmPassword}
+											onChange={this.onChange}
+											type="password"
+											name="confirmPassword"
+											className="form-control"
+										/>
+										{errors === 'Confirm password field is required' && <div className='alert alert-danger'>{errors}</div>}
+									</div>
+								</div>
+								<div className="form-group row">
+									<div className="col-sm-6">
+									<label className='control-label'>Sex</label>
+									<select
+										className='form-control'
+										name='sex'
+										onChange={this.onChange}
+										value={sex}
+									>
+										<option value='' disabled>Choose</option>
+										<option value='male'>Male</option>
+										<option value='female'>Female</option>
+									</select>
+									{errors === 'Sex is required' && <div className='alert alert-danger'>{errors}</div>}
+									</div>
+									<div className="col-sm-6">
+									<label className='control-label'>Profile Picture</label><br/>
+									<input
+										type="file"
+										onChange={this.fileChange.bind(this)}
+									/>
+									<progress value={uploadProgress} max="100" />
+									</div>
+								</div>
+								<button
+									id="submitButton"
+									disabled={isLoading}
+									className="btn btn-orange btn-lg"
+								>
+									Sign Up
+								</button>
+								<p id="signup-link">Already have an account?<span><Link to="/signin"> Sign In</Link></span></p>
+							</form>
 						</div>
-						<div>
-							<label className='control-label'>Last Name</label>
-							<input
-								value={lastname}
-								onChange={this.onChange}
-								type="text"
-								name="lastname"
-								className="form-control"
-							/>
-							{errors === 'Lastname is required' && <div className='alert alert-danger'>{errors}</div>}
-						</div>
-						<div>
-							<label className='control-label'>Username</label>
-							<input
-								value={username}
-								onChange={this.onChange}
-								type="text"
-								name="username"
-								className="form-control"
-							/>
-							{errors === 'Username is required' && <div className='alert alert-danger'>{errors}</div>}
-						</div>
-						<div>
-							<label className='control-label'>Email</label>
-							<input
-								value={email}
-								onChange={this.onChange}
-								type="email"
-								name="email"
-								className="form-control"
-							/>
-							{errors === 'Email is required' && <div className='alert alert-danger'>{errors}</div>}
-						</div>
-						<div>
-							<label className='control-label'>Password</label>
-							<input
-								value={password}
-								onChange={this.onChange}
-								type="password"
-								name="password"
-								className="form-control"
-							/>
-							{errors === 'Password is required' && <div className='alert alert-danger'>{errors}</div>}
-							{errors === 'Minimum password length is 5 characters' && <div className='alert alert-danger'>{errors}</div>}
-						</div>
-						<div>
-							<label className='control-label'>Confirm Password</label>
-							<input
-								value={confirmPassword}
-								onChange={this.onChange}
-								type="password"
-								name="confirmPassword"
-								className="form-control"
-							/>
-							{errors === 'Confirm password field is required' && <div className='alert alert-danger'>{errors}</div>}
-						</div>
-						<div>
-							<label className='control-label'>Sex</label>
-							<select
-								className='form-control'
-								name='sex'
-								onChange={this.onChange}
-								value={sex}
-							>
-								<option value='' disabled>Choose</option>
-								<option value='male'>Male</option>
-								<option value='female'>Female</option>
-							</select>
-							{errors === 'Sex is required' && <div className='alert alert-danger'>{errors}</div>}
-						</div>
-						<div>
-							<label className='control-label'>Profile Picture</label>
-							<input
-								type="file"
-								onChange={this.fileChange.bind(this)}
-							/>
-							<progress value={uploadProgress} max="100" />
-						</div>
-						<button
-							id="signup"
-							disabled={isLoading}
-							className="btn btn-orange btn-lg"
-						>
-							Sign Up
-						</button>
-					</form>
+					</div>
 				</div>
 			</div>
 		);
