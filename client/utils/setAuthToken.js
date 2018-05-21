9b64af0907fc5247b3b0d89969519c895facd87b
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2018-05-21 17:43:55 +0100

    Adds JS Doc to file

--- client/utils/setAuthToken.js
+++ client/utils/setAuthToken.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
+/**
+ * @description - Sets token to headers
+ * @param {*} token
+ * @returns {Object} token
+ */
 export default function setAuthToken(token) {
-	if (token) {
-		axios.defaults.headers.common['Authorization'] = `${token}`;
-	} else {
-		delete axios.defaults.headers.common['Authorization'];
-	}
+  if (token) {
+    axios.defaults.headers.common.Authorization = `${token}`;
+  } else {
+    delete axios.defaults.headers.common.Authorization;
+  }
 }
