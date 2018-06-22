67c99943b19e52a3321e862498cf1b21ca211589
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2018-06-22 09:42:22 +0100

    Setting up mock data for tests

---
+++ client/__test__/mocks/mockData.js
@@ -0,0 +1,11 @@
+const mockData = {
+  businessData: {
+    id: 1,
+    businessName: 'Microsoft',
+    category: 'technology',
+    email: 'microsoft.com',
+    location: 'United States'
+  },
+};
+
+export default mockData;
